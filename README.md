c32fcbb456dd939e28cf87483ede494d3c89d890
Steren <steren.giannini@gmail.com>
2022-06-03 04:43:48 +0000

    Initial health check helper

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # healthchecks
 A Node.js app to test startup and liveness health checks
+
+## How to use
+
+### Testing http probes
+
+Use URL parameters to customize the response to requests sent on any path of this server.
+
+* delaySeconds: The number of seconds to wait before returning, defaults to 0.
+* status: The HTTP status code to return, default to 200.
+* failures: The number of failures (500 responses) to return before returning the status above, default to 0.
+
+
+* `/healthcheck?delaySeconds=10&status=500`: will return a 500 status code with a delay of 10 seconds
+* `/?failures=5`: will return a 5 500 responses followed by 1 200 response
+
+### Testing TCP probes
+
+Use environment variables to customize the time before the server listens on the port.
+
+* `SERVER_DELAY_SECONDS` (defaults to 0): The number of seconds to wait before listening on the port.
+* `SERVER_PORT` (defaults to `PORT`, or to 8080 if not present): The port to listen on.
---
+++ index.js
@@ -0,0 +1,41 @@
+import * as http from 'node:http';
+
+const port = process.env.SERVER_PORT || process.env.PORT || 8080;
+const serverDelay = parseInt(process.env.SERVER_DELAY_SECONDS) || 0;
+let failureCounter = 0;
+
+console.info(`server will listen on port ${port} after ${serverDelay} seconds.`)
+
+const server = http.createServer(async (req, res) => {
+  const url = new URL(req.url, `http://example.com`);
+  const delaySeconds = parseInt(url.searchParams.get('delaySeconds')) || 0;
+  const status = parseInt(url.searchParams.get('status')) || 200;
+  const failures = parseInt(url.searchParams.get('failures')) || 0;
+
+  // ignore favicon requests
+  if (req.pathname === '/favicon.ico') {
+    res.statusCode = 404;
+    res.end();
+    return;
+  }
+
+  console.log(`Will return ${status} in ${delaySeconds} seconds after ${failures} failures (currently at ${failureCounter}/${failures} failures).`);
+
+  if (failureCounter < failures) {
+    failureCounter++;
+    res.statusCode = 500;
+    res.end();
+    return;
+  }
+  failureCounter = 0;
+
+  setTimeout(() => {
+    res.statusCode = status;
+    res.end();
+  }, delaySeconds * 1000);
+
+});
+
+setTimeout(() => {
+  server.listen(port, () => console.info(`server is listening on port ${port} after ${serverDelay} seconds.`));
+}, serverDelay * 1000);
